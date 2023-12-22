03219f59d4101387742dac75df8c4721e8fb0ada
Brent Laminack <brent@laminack.com>
2023-12-22 16:14:26 -0500

    Update orgs_ab2.sql working on cast

--- orgs/models/generated/airbyte_ctes/dbo/orgs_ab2.sql
+++ orgs/models/generated/airbyte_ctes/dbo/orgs_ab2.sql
@@ -6,9 +6,9 @@
 -- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
 -- depends_on: {{ ref('orgs_ab1') }}
 select
-    cast(_id as {{ type_json() }}) as _id,
+    cast(_id as varchar(500) ) as _id,
     cast(num as {{ dbt_utils.type_float() }}) as num,
-    cast(name as nvarchar(500) ) as name,
+    cast(name as varchar(500) ) as name,
     steps,
     _airbyte_ab_id,
     _airbyte_emitted_at,
